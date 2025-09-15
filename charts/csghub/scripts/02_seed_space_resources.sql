9a310f4525da94812d844cf0634e569ef1654405
mason <xiang.ma@opencsg.com>
2025-09-15 12:18:52 +0800

    refactor: update resource seed sql file to adapt runner

--- charts/csghub/scripts/02_seed_space_resources.sql
+++ charts/csghub/scripts/02_seed_space_resources.sql
@@ -27,28 +27,55 @@ SELECT pg_catalog.set_config('search_path', 'public', false);
 -- Seed Data for Name: space_resources; Type: TABLE DATA; Schema: public; Owner: csghub
 --
 
-WITH selected_cluster AS (
-    SELECT
-        cluster_id
-    FROM
-        public.cluster_infos
-    LIMIT 1)
-    INSERT INTO public.space_resources (id, name, resources, cluster_id)
-        VALUES
-        (1, 'CPU basic · 0.5 vCPU · 1 GB', '{ "cpu": { "type": "Intel", "num": "0.5" }, "memory": "1Gi" }', ( SELECT cluster_id FROM selected_cluster)),
-        (2, 'CPU basic · 2 vCPU · 4 GB', '{ "cpu": { "type": "Intel", "num": "2" }, "memory": "4Gi" }', ( SELECT cluster_id FROM selected_cluster))
+INSERT INTO public.space_resources (id, name, resources, cluster_id)
+SELECT
+    1,
+    'CPU basic · 0.5 vCPU · 1 GB',
+    '{ "cpu": { "type": "Intel", "num": "0.5" }, "memory": "1Gi" }',
+    COALESCE((SELECT cluster_id FROM public.cluster_infos LIMIT 1), '')
+UNION ALL
+SELECT
+    2,
+    'CPU basic · 2 vCPU · 4 GB',
+    '{ "cpu": { "type": "Intel", "num": "2" }, "memory": "4Gi" }',
+    COALESCE((SELECT cluster_id FROM public.cluster_infos LIMIT 1), '')
 ON CONFLICT (id)
-    DO UPDATE SET
-        name = EXCLUDED.name,
-        resources = EXCLUDED.resources,
-        cluster_id = EXCLUDED.cluster_id;
+DO UPDATE SET
+    name = EXCLUDED.name,
+    resources = EXCLUDED.resources,
+    cluster_id = CASE
+        WHEN EXCLUDED.cluster_id != '' THEN EXCLUDED.cluster_id
+        ELSE space_resources.cluster_id
+    END;
+
+--
+-- Create Trigger Function
+--
+CREATE OR REPLACE FUNCTION update_space_resources_cluster_id()
+RETURNS TRIGGER AS $$
+BEGIN
+    -- Update cluster_id for id 1,2
+    UPDATE public.space_resources
+    SET cluster_id = NEW.cluster_id
+    WHERE cluster_id = ''
+    AND id IN (1, 2);
+
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+--
+-- Create Trigger
+--
+DROP TRIGGER IF EXISTS trg_update_space_resources ON public.cluster_infos;
+
+CREATE TRIGGER trg_update_space_resources
+    AFTER INSERT ON public.cluster_infos
+    FOR EACH ROW
+    EXECUTE FUNCTION update_space_resources_cluster_id();
 
 --
 -- Name: space_resources_id_seq; Type: SEQUENCE SET; Schema: public; Owner: csghub
 --
-
-SELECT
-    pg_catalog.setval('public.space_resources_id_seq', (
-            SELECT
-                MAX(id)
-            FROM public.space_resources), TRUE);
+SELECT pg_catalog.setval('public.space_resources_id_seq', (
+    SELECT MAX(id) FROM public.space_resources), TRUE);
